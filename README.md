86b6e902d972fcfd85332a177c96b1feb532af61
Pedro Lucas <pedro99lucasom@gmail.com>
2021-07-06 02:28:55 -0300

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ConditionalSpringBatch
+
 :computer: Spring Batch Application to show like steps conditions :question: work.
---
+++ src/test/java/com/system/conditionalspringbatch/ConditionalSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.conditionalspringbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConditionalSpringBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
